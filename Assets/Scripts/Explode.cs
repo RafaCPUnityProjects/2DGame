5c4f258c80b4005a640f8d284539d5501e980987
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2014-07-07 20:43:31 -0300

    Lots of progress!

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+	public BodyPart bodyPart;
+	public int totalParts = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D target)
+	{
+		if (target.gameObject.tag == "Deadly")
+		{
+			OnExplode();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D target)
+	{
+		if (target.gameObject.tag == "Deadly")
+		{
+			OnExplode();
+		}
+	}
+
+	public void OnExplode()
+	{
+		Destroy(this.gameObject);
+		
+		Transform t = transform;
+		for (int i = 0; i < totalParts; i++)
+		{
+			t.TransformPoint(0, -100, 0);
+			BodyPart clone = Instantiate(bodyPart, t.position, Quaternion.identity) as BodyPart;
+			clone.rigidbody2D.AddForce(Vector3.right * Random.Range(-50, 50));
+			clone.rigidbody2D.AddForce(Vector3.up * Random.Range(100, 400));
+			clone.rigidbody2D.AddForce(this.rigidbody2D.velocity * 100);
+			//Debug.Log("Velocity: " + this.rigidbody2D.velocity);
+		}
+	}
+}
